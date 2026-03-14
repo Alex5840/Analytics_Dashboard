fb4515e353992445e549b46cc8c6a563e1232dc1
Alex <alexdennis584@gmail.com>
2026-03-14 16:28:48 +0530

    fix frontend api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const API = axios.create({
+const api = axios.create({
   baseURL: "https://analytics-dashboard-0z04.onrender.com/api"
 })
 
-export default API
+export default api
